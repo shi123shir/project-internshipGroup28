2afe62a637aa22e62c3fc3bc7d833b6bbe882270
shishir sharma <shishirsharma453@gmail.com>
2022-09-14 16:10:50 +0530

    hi

--- src/route/route.js
+++ src/route/route.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router  = express.Router()
 const collageController = require("../Controller/CollegeController")
+const getcontroller = require("../Controller/getController")
 
 
 router.post("/functionup/colleges",collageController.createcollage)
 
-
+router.get("/functionup/collegeDetails",getcontroller.getcollegeDetails)
 
 
 
