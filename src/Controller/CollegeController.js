32489a6f457e2101c19c4162705bd0fa55675d8f
shishir sharma <shishirsharma453@gmail.com>
2022-09-14 14:53:57 +0530

    hello

--- src/Controller/CollegeController.js
+++ src/Controller/CollegeController.js
@@ -1,10 +1,10 @@
 const collegeModel = require("../Model/collageModel")
-const isvalid = require ("validator")
-const { model } = require("mongoose")
+const validator = require ("validator")
 
 const createcollage = async function(req,res){
     try {
-        const nameregex = /^[a-z\s]+$/i
+        const nameregex = /^[a-zA-Z ]*$/;
+        
         const collage = req.body
         if(Object.keys(collage)==0)return res.status(400).send({status:false,message:"body required"})
 
@@ -13,15 +13,18 @@ const createcollage = async function(req,res){
         if(!collage.name.match(nameregex)) return res.status(400).send({status:false , message:"name is not valid format"})
 
         if(!collage.fullName) return res.status(400).send({status:false,message:"fullName required"})
+        if(!collage.fullName.match(nameregex)) return res.status(400).send({status:false, message:"fullName must in valid format"})
 
         if(!collage.logoLink) return res.status(400).send({status:false,message:"logolink is required"})
 
+        if (!validator.isURL(collage.logoLink)) { return res.status(400).send({ status: false, msg: "logoLink should be valid URL" })}
+      
         let collagecreate = await collegeModel.create(collage)
 
-        res.status(201).send({status:true,message:"collage created" ,data:collagecreate})
+        res.status(201).send({status:true,message:"collage created successfully" ,data:collagecreate})
 
     } catch (err) {
-        res.status.send({status:false,error:err.message})
+        res.status(500).send({status:false,error:err.message})
     }
 }
 
